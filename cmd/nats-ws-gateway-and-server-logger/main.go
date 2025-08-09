ba1da2864c54f89d4fe8214011e0ffd856dba1dc
Dave Bevan <dave.bevan@evobytes.co.uk>
2025-08-09 22:13:40 +0100

    tweak to always release on individual app build

--- cmd/nats-ws-gateway-and-server-logger/main.go
+++ cmd/nats-ws-gateway-and-server-logger/main.go
@@ -97,7 +97,7 @@ func main() {
 		slog.Error("❌ Failed to subscribe to NATS", "topic", ">", "err", err)
 		os.Exit(1)
 	}
-	slog.Info("🎧 Subscribed to all available topics (`>`). Logging traffic to", logFilePath)
+	slog.Info("🎧 Subscribed to all available topics using (`>`). Logging traffic to", logFilePath)
 
 	// --- Graceful Shutdown ---
 	stop := make(chan os.Signal, 1)
